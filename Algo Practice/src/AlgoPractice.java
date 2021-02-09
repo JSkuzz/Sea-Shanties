e3f7f8037cccf5ed4a76cf6ed53f17ac8bd99cd6
Justin Skoczylas <jcskoczylas@gmail.com>
2021-02-08 19:08:15 -0800

    do these

---
+++ Algo Practice/src/AlgoPractice.java
@@ -0,0 +1,35 @@
+
+public class AlgoPractice {
+
+	public static void main(String[] args) {
+		//Starting from scratch
+		
+		//Setting and Swapping
+		//Set myNumber to 42, set myName to name, then swap the values
+		
+		
+		//Print -52 to 1066 using a for loop
+		
+		//Create beCheerful(). console.log "good morning" and call the function 98 times
+		
+		//multiples  of three - but not all
+		//using a for loop print multiples of 3 from -300 to 0. skip -3 and -6.
+		
+		//print integers from 2000 to 5800 using a while loop
+		
+		//if 2 given numbers represent your birth month and day in either  order console.log "how did you  know, elseif log "just another day"
+		
+		//print multiples of 6 up to 60,000 using a while loop
+		
+		//print 1 to 100. if /5 log "go". if /10 also log "cougs"
+		
+		//write a funciton that accepts param "incoming" and log the value
+		
+		//add odd integers from -300k to 300k and log the final sum
+		
+		//log positive numbers  starting at 2016 - 0 by 4s without a for loop
+		
+		
+	}
+
+}
